80c8078372425aea1c45476725b5bf4ff17ef651
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-02 13:15:44 -0500

    added project header

--- MAPD724-W2022-MidTerm-301287641/Core/GameObject.swift
+++ MAPD724-W2022-MidTerm-301287641/Core/GameObject.swift
@@ -1,9 +1,18 @@
-//
-//  GameObject.swift
-//  MAPD724-W2022-MidTerm-301287641
-//
-//  Created by Wilson Mungai on 2023-03-01.
-//
+/*
+ 
+ MAPD 724 Mail Pilot
+ 
+ Mid Term Test
+ 
+ Muguthi, Wilson Mungai / Student Number: 301287641
+ 
+ A game built with SpriteKit and UIKit. A game plane where the player has to control a plane to maneuver over islands to acuumulate points. The player has to avoid colliding with clouds which results in losing lives. The player has 5 lives. There are three scenes; start scene, game over scene, and game over scene.
+ 
+ Version 1
+ 
+ Last modified: 02/March/2023
+ 
+ */
 
 import GameplayKit
 import SpriteKit
